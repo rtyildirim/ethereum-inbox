69622e1b7d0d344133f03372ecbbc851bd328843
Tolga Yildirim <tolga.yildirim@gmail.com>
2021-11-11 17:47:17 -0500

    Add more test coverage for inbox

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,6 +6,7 @@ const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+const initialMessage = "Hi there";
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -13,12 +14,19 @@ beforeEach(async () => {
 
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there"] })
+    .deploy({ data: bytecode, arguments: [initialMessage] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    //console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+
+    assert.equal(initialMessage, message);
   });
 });
